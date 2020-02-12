1072494a7e9558a683632a3b59e9a028d162af9e
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-02-12 11:59:55 -0700

    Add test of button click

--- src/ReservationCard/ReservationCard.test.js
+++ src/ReservationCard/ReservationCard.test.js
@@ -10,6 +10,22 @@ it('should be an instance of an ReservationCard componenet', () => {
     date='02/12'
     time='7:00'
     number={15}
+    deleteReservation={jest.fn()}
   />);
   expect(wrapper).toMatchSnapshot();
+})
+
+it('should call addReservation when button is clicked', () => {
+  const deleteReservationMock = jest.fn();
+  const wrapper = shallow(
+    <ReservationCard 
+    id={1}
+    name='Caleb'
+    date='02/12'
+    time='7:00'
+    number={15}
+    deleteReservation={deleteReservationMock}
+  />);
+  wrapper.find('button').simulate('click')
+  expect(deleteReservationMock).toHaveBeenCalled();
 })

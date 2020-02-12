29a0609dfd2bc7ba4bf286d40e527f5cd334d32d
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-02-12 10:20:42 -0700

    Add state, data, and components to app

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
+import Reservations from '../Reservations/Reservations'
+import Form from '../Form/Form'
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservations: []
+    }
+  }
+
+  addReservation = (newRes) => {
+    this.setState({reservations: [...this.state.reservations, newRes]})
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/reservations')
+    .then(response => response.json())
+    .then(reservations => this.setState({reservations}))
+  }
+
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+          <Form addReservation={this.addReservation} />
         </div>
         <div className='resy-container'>
-          
+          <Reservations reservations={this.state.reservations} />
         </div>
       </div>
     )

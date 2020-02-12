7b1e93e9f3bc6d214f29f23cfd92bcd6a965ffee
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-02-12 11:59:36 -0700

    Add delete to cancel button

--- src/ReservationCard/ReservationCard.js
+++ src/ReservationCard/ReservationCard.js
@@ -11,16 +11,13 @@ class ReservationCard extends Component {
   }
 
   render() {
-    console.log(this.props.id);
-    
     return (
       <div className='res-card'>
         <h3>{this.props.name}</h3>
           <p>{this.props.date}</p>
           <p>{this.props.time}</p>
           <p>Number of guests: {this.props.number}</p>
-
-        <button>Cancel</button>
+        <button className='res-btn' onClick={() => this.props.deleteReservation(this.props.id)}>Cancel</button>
       </div>
     )
   }

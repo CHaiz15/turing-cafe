28608c51bd77b4c8c08716a5d7db90c9c6b957c4
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-02-12 11:58:58 -0700

    Add tests to form

--- src/Form/Form.test.js
+++ src/Form/Form.test.js
@@ -3,12 +3,20 @@ import Form from './Form';
 import { shallow } from 'enzyme'
 
 it('should be an instance of an Form componenet', () => {
-  const wrapper = shallow(<Form />);
+  const wrapper = shallow(
+    <Form 
+      addReservation={jest.fn()}
+    />
+  );
   expect(wrapper).toMatchSnapshot();
 });
 
 it('should change state when handleChange is called', () => {
-  const wrapper = shallow(<Form />);
+  const wrapper = shallow(
+    <Form 
+      addReservation={jest.fn()}
+    />
+  );
   const mockEvent = {
     preventDefault: jest.fn(),
     target: {
@@ -19,3 +27,26 @@ it('should change state when handleChange is called', () => {
   expect(wrapper.state('name')).toEqual('Richie')
 })
 
+it('should call addReservation when button is clicked', () => {
+  const addReservationMock = jest.fn();
+  const wrapper = shallow(
+    <Form 
+      addReservation={addReservationMock}
+    />
+  );
+  wrapper.find('.form-btn').simulate('click')
+  expect(addReservationMock).toHaveBeenCalled();
+})
+
+it('should change state when handleClick is called', () => {
+  const wrapper = shallow(
+    <Form 
+      addReservation={jest.fn()}
+    />
+  );
+  const defaultState = {name: 'Jeff', date: '04/23', time: '7:00', number: '16'}
+  const expected = {name: '', date: '', time: '', number: ''}
+  wrapper.instance().setState(defaultState);
+  wrapper.instance().handleClick();
+  expect(wrapper.state()).toEqual(expected);
+})
